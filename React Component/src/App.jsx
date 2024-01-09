9d0e4b0ac281a4cd679ef1dc44a4c567b1577aba
jayapradhajayaraj <38317505+jayapradhajayaraj@users.noreply.github.com>
2024-01-09 00:55:30 +0000

    Add files via upload

---
+++ React Component/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./heading.jsx";
+import List from "./list.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+
+export default App;
